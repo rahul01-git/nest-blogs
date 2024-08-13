687370d504468994daacf36e5070d6fbfe57d5c9
rahul01-git <rahul.neupane@fleetpanda.com>
2024-08-13 16:50:18 +0545

    login and signup feature done

---
+++ src/modules/users/user.entity.ts
@@ -0,0 +1,36 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+@Table({
+  tableName: 'users',
+  timestamps: true,
+  paranoid: true,
+  freezeTableName: true,
+  underscored: true,
+})
+export class User extends Model<User> {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  name: string;
+
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  email: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password: string;
+
+  @Column({
+    type: DataType.ENUM,
+    values: ['male', 'female'],
+    allowNull: false,
+  })
+  gender: string;
+}
