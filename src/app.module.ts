a3dc72d0b326b6cbc18a0944c83617e39947c4c0
rahul01-git <rahul.neupane@fleetpanda.com>
2024-08-14 10:51:23 +0545

    crud posts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './modules/auth/auth.module';
     DatabaseModule,
     UsersModule,
     AuthModule,
+    PostsModule,
   ],
 })
 export class AppModule {}
---
+++ src/modules/posts/post.entity.ts
@@ -0,0 +1,40 @@
+import {
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { User } from '../users/user.entity';
+
+@Table({
+  tableName: 'posts',
+  freezeTableName: true,
+  underscored: true,
+  timestamps: true,
+  paranoid: true,
+})
+export class Post extends Model<Post> {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  title: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  body: string;
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  userId: number;
+
+  @BelongsTo(() => User)
+  user: User;
+}
